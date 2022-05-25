76453bc84823a07f76ab21f3511d8408c495bc59
Gal Naim <dorbanp@gmail.com>
2022-05-25 18:37:45 +0300

    first commit

---
+++ main20SearchBar.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector(".menu");
+const inputWindow = document.querySelector(".input-Window");
+let fruits = ["apple", "banana", "orange", "peach", "applebees", "bananasplit"];
+
+function displayitems(arr) {
+  menu.textContent = "";
+  if (arr.length === 0) {
+    const noResultsMessege = document.createElement("li");
+    noResultsMessege.textContent = "No Results Found";
+    menu.append(noResultsMessege);
+  } else {
+    arr.forEach(function (item) {
+      console.log(item);
+      const liItem = document.createElement("li");
+      liItem.textContent = item;
+      menu.append(liItem);
+    });
+  }
+}
+displayitems(fruits);
+
+
+inputWindow.oninput = checkwords;
+function checkwords() {
+  let input = inputWindow.value;
+  let filtered = fruits.filter(function (el) {
+    return el.includes(input);
+  });
+
+  displayitems(filtered);
+}
